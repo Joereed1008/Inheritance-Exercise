63cf3d7ca0070d86fa6a51b6466f79b0d7b97066
Joseph <Joseph@DESKTOP-G5ITN32>
2023-03-18 20:03:32 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Inheritance_Exercise
+{
+    internal class Program : Stack
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push("This is a stack");
+            stack.Push(2.32);
+            stack.Push("It takes loads of object types!");
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+            stack.Clear();
+
+            //boxing unboxing occurs due to use of arraylist
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Inheritance_Exercise
+{
+    public class Stack
+    {
+        ArrayList stacks = new ArrayList();
+        public Stack()
+        {
+
+        }
+        public void Push(object pushed)
+        {
+            if (pushed != null) { stacks.Add(pushed); }
+            else
+                throw new Exception("Fucking null? nob");
+        }
+        public void Pop()
+        {
+            if (stacks.Count > 0)
+            {
+                Console.WriteLine(stacks[stacks.Count - 1]);
+                stacks.RemoveAt(stacks.Count - 1);
+            }
+        }
+        public void Clear() { ArrayList stacks = new ArrayList(); }
+    }
+}
